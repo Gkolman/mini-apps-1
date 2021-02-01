2f737603c790d8a3d595bf6e7bb9e13123f8e207
gkolman <gkolman17@gmail.com>
2021-01-31 19:44:38 -0800

    finished mini apps 2

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,48 @@
 
+var parseData = (jsonData) => {
+  if (!jsonData) { return }
+  if (jsonData[0] !== '{') return
+  console.log('parseData is working')
+  var newData = JSON.parse(jsonData)
+  var data = { col : [], row : []}
+  for (var key in newData) { if (key !== 'children') { data.col.push(key) } }
+  var recurse = (parent) => {
+    data.row.push([])
+    for ( var key in parent ) { if (key !== 'children') { data.row[data.row.length-1].push(parent[key]) }}
+    if(parent.children.length > 0) { for (var child of parent.children) { recurse(child)} }
+  }
+  recurse(newData)
+  return data
 
+}
+
+var tablefyData = (data) => {
+  console.log('data -> ', data)
+  var template = ` <table><thead><tr>`
+    for ( var col of data.col) { template += `<th>${col}</th>` }
+  template += `</tr> </thead <tbody>`
+  for ( var row of data.row) {
+    template += `<tr>`
+    for ( var data of row) { template += `<td>${data}</td>` }
+    template += `</tr>`
+  }
+  template += `</tbody> </table>`
+  return template
+}
+
+var formTemplate =
+`
+  <h1 id = 'working' >Post JSON below</h1>
+  <form action="/upload_json" name="jsonText" method="post">
+      <textarea id="jsonData" class="text" cols="86" rows ="20" name="jsonData"></textarea>
+     <input id = 'button' type="submit" value="translate" class="submitButton">
+  </form>
+  <div id = 'afterData'>`
+
+
+module.exports = {
+  parseData : parseData,
+  tablefyData: tablefyData,
+  formTemplate : formTemplate
+}
+// module.exports = tablefyData
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,76 @@
+const express = require('express');
+const app = express();
+
+var port = '5000'
+var parseData = require('./client/app.js').parseData
+var tablefyData = require('./client/app.js').tablefyData
+var formTemplate = require('./client/app.js').formTemplate
+// var router = express.Router();
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static('client'))
+
+app.listen(port, () => {
+  console.log(`listening to ${port}`)
+})
+app.get("/", (req, res) => {
+  console.log('req -> ', 'working' )
+
+  res.end();
+});
+app.get("/upload_json", (req, res) => {
+  console.log('get("/upload_json"')
+
+
+  res.end();
+});
+
+app.post("/upload_json", (req, res) => {
+
+var parsed = parseData(req.body.jsonData)
+
+var template = formTemplate
+
+console.log(' req.body -> ', req.body)
+
+if (parsed) {
+  template += `${tablefyData(parsed)}</div>`
+  res.send(template)
+} else {
+  template += ' no JSON data</div>'
+  res.send(template)
+}
+
+
+  // console.log('response -> ', res)
+
+
+// perform the parse data function
+
+// save the parsed data in a variable
+//
+
+// console.log(res)
+
+
+
+// res.redirect('/')
+
+
+// res.sendFile(__dirname + '/secondIndex.html')
+
+// perform
+
+});
+
+
+
+
+
+var htmlForm = `<h1 id = 'working' >Post JSON below</h1>
+                <form action="/upload_json" name="jsonText" method="post">
+                <textarea id="jsonData" class="text" cols="86" rows ="20" name="jsonData"></textarea>
+                <input id = 'button' type="submit" value="translate" class="submitButton">
+                </form>`
 
 
