1b94f93bf9804315ebdc9095d971c694e8e8c6c1
gkolman <gkolman17@gmail.com>
2021-07-21 15:45:31 -0700

    esited JSON parse

---
+++ challenge_2/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+
+var port = '5000'
+var { parseData, tablefyData, formTemplate} = require('./client/app.js')
+// var router = express.Router();
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static('client'))
+
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+
+
+app.listen(port, () => {
+  console.log(`app is running on http://localhost:${port}`)
+})
+app.get("/", (req, res) => {
+  console.log('req -> ', 'working' )
+
+  res.end();
+});
+app.get("/upload_json", (req, res) => {
+  console.log('get("/upload_json"')
+  // res.sendFile(__dirname './client/index.html');
+  res.end()
+});
+
+app.post("/uploadText", (req, res) => {
+  var parsed = parseData(req.body.jsonData)
+  if (parsed) {
+    formTemplate += `${tablefyData(parsed)}</div>`
+    res.send(formTemplate)
+  } else {
+    formTemplate += ' no JSON data</div>'
+    res.send(formTemplate)
+  }
+});
+
+app.post('/uploadFile', upload.single('avatar'), function (req, res, next) {
+  console.log('req.file -> ', req.file)
+  fs.readFile(req.file.path, 'utf8' , (err, data) => {
+    if (err) {console.error(err);return}
+    var parsed = parseData(data)
+    if (parsed) {
+      formTemplate += `${tablefyData(parsed)}</div>`
+      res.send(formTemplate)
+    } else {
+      formTemplate += ' no JSON data</div>'
+      res.send(formTemplate)
+    }
+  });
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
+
+
+
+
+
+
+
+
+var htmlForm = `<h1 id = 'working' >Post JSON below</h1>
+                <form action="/upload_json" name="jsonText" method="post">
+                <textarea id="jsonData" class="text" cols="86" rows ="20" name="jsonData"></textarea>
+                <input id = 'button' type="submit" value="translate" class="submitButton">
+                </form>`
+
+
